c948530d29453e81281c44805afcaeccd7eeecdf
Yujie Wu <coffee377@dingtalk.com>
2026-05-28 19:05:31 +0800

    chore(gradle): 升级 Gradle 9.4.0 与 Lombok 1.18.46
    - Gradle 8.14.4 -> 9.4.0
    - Lombok 1.18.42 -> 1.18.46
    - 简化 settings.gradle.kts，使用 composite build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,12 @@
 rootProject.name = "jtools"
 
 pluginManagement {
-    repositories {
-        mavenLocal()
-        maven {
-            url = uri("https://packages.aliyun.com/5f6a9b06d24814603933faab/maven/2038604-snapshot-xnrepo")
-            credentials {
-                val aliyunMavenUsername: String =
-                    System.getProperty("dev.opts.aliyun.maven.username", "5f4ba059fa82bfeb805a1e09");
-                val aliyunMavenPassword: String = System.getProperty("dev.opts.aliyun.maven.password", "a3XkZLNApybs");
-
-                username = aliyunMavenUsername
-                password = aliyunMavenPassword
-            }
-        }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        gradlePluginPortal()
-    }
+    includeBuild("/Users/wuyujie/Project/opensource/unity/gradle-plugins")
 }
 
 plugins {
-    id("ccl.repo") version "0.10.0-beta.3"
+    id("ccl.repo")
+//    id("ccl.repo") version "0.10.0-beta.3"
 //    id("io.github.sgtsilvio.gradle.proguard") version "0.8.0" apply false
 }
 
